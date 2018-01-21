b2521c258e7db5f37ef4a56d2d2dd38189b6a4be
Phillip <phillipables1@gmail.com>
2018-01-21 11:31:24 -0500

    annotation to bing Gui Controller with the Scene

--- Remedy/src/GuiController.java
+++ Remedy/src/GuiController.java
@@ -3,7 +3,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -13,6 +17,15 @@ public class GuiController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-    }    
+    }
+    @FXML
+    Button addButton;
+    @FXML
+    DatePicker datePicker;
+    @FXML
+    TextField descriptionTextField; 
+    @FXML
+    ListView eventList;
+    
     
 }
